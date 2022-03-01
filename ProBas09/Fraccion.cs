26278ea3b24704a461d722742043ae7760e6aef9
fongdubon <eduardofongdubon@hotmail.com>
2022-03-01 14:47:27 -0600

    Crear clase fraccion

---
+++ ProBas09/Fraccion.cs
@@ -0,0 +1,26 @@
+﻿namespace ProBas09
+{
+    public class Fraccion
+    {
+        #region Propiedades
+        public int Numerador { get; set; }
+        public int Denominador { get; set; }
+        #endregion
+        #region Métodos
+        public Fraccion()
+        {
+            this.Numerador = 0;
+            this.Denominador = 1;
+        }
+        public Fraccion(int numerador, int denominador)
+        {
+            this.Numerador = numerador;
+            this.Denominador = denominador;
+        }
+        public Fraccion Multiplicacion(Fraccion B)
+        {
+            return new Fraccion(this.Numerador * B.Numerador, this.Denominador * B.Denominador);
+        }
+        #endregion
+    }
+}
